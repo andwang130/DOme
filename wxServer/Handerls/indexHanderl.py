ea9d6a4aef56c532159ad5504f250a650ca3af00
andwang130 <627656470@qq.com>
2018-11-07 07:00:00 +0800

    update

--- wxServer/Handerls/indexHanderl.py
+++ wxServer/Handerls/indexHanderl.py
@@ -7,6 +7,7 @@ import tornado
 import tornado.httpclient
 import json
 class indexHanderl(Basehanderl.Basehandelr):
+    @tornado.gen.coroutine
     def get(self):
         uuid=self.get_argument("uuid",None)
         code = self.get_argument("code",None)
@@ -15,7 +16,7 @@ class indexHanderl(Basehanderl.Basehandelr):
         if code:
             if not openid:
                 newopenid=yield self.get_openid(code)
-            
+
                 self.set_secure_cookie("openid",newopenid)
             if uuid:
                 self.db_linck()
