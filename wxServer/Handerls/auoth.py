911f1c20baa4e4ac835829e11e8aa9b0a415f0fb
andwang130 <627656470@qq.com>
2018-11-06 14:59:53 +0800

    up

--- wxServer/Handerls/auoth.py
+++ wxServer/Handerls/auoth.py
@@ -6,7 +6,8 @@ class auoth(Basehandelr):
         uuid=self.get_argument("uuid")
         values=www+"wx/wxindex?uuid={}".format(uuid)
         data = urlparse(values)
-        url="https://open.weixin.qq.com/connect/oauth2/authorize?appid={}&redirect_uri={}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect".format(wxcongif["appId"],data)
+        link = urljoin(data.scheme + "://" + data.netloc, data.path)
+        url="https://open.weixin.qq.com/connect/oauth2/authorize?appid={}&redirect_uri={}&response_type=code&scope=SCOPE&state=STATE#wechat_redirect".format(wxcongif["appId"],link)
         print(url)
         self.redirect(url)
 
