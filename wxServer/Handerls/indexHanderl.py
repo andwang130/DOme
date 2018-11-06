b866e28828e5f4ae697b94c9b39a7da7a6b64b21
andwang130 <627656470@qq.com>
2018-11-07 03:14:24 +0800

    update

--- wxServer/Handerls/indexHanderl.py
+++ wxServer/Handerls/indexHanderl.py
@@ -9,47 +9,48 @@ class indexHanderl(Basehanderl.Basehandelr):
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
-        uuid=self.get_argument("uuid")
         code = self.get_argument("code")
         if code:
             url = ' https://api.weixin.qq.com/sns/oauth2/access_token?appid={}&secret={}&code={}&grant_type=authorization_code'.format(pojcetm.wxcongif["appId"],pojcetm.wxcongif["secret"],code)
             print(url)
             http_client = tornado.httpclient.AsyncHTTPClient()
-            response = yield tornado.gen.Task(http_client.fetch(url),validate_cert=False)
-            print(response.code)
+            http_client.fetch(url,self.on_callback)
+
         else :
             return
+
+    def on_callback(self,response):
+        print(response.code)
+        uuid = self.get_argument("uuid")
         if uuid:
             self.db_linck()
 
-            coures=self.Mongodb["poject"].find_one({"uuid":uuid})
-            self.Mongodb["poject"].update_one({"uuid":uuid},{"$inc": {"volume": 1}})
-            count=self.Mongodb["tpUser"].find({"uuid":uuid}).count()
+            coures = self.Mongodb["poject"].find_one({"uuid": uuid})
+            self.Mongodb["poject"].update_one({"uuid": uuid}, {"$inc": {"volume": 1}})
+            count = self.Mongodb["tpUser"].find({"uuid": uuid}).count()
             if coures:
-                data={}
-                data["count"]=count
-                data["endtimes"]=time.mktime(time.strptime(coures["votestart"], '%Y-%m-%d %H:%M'))-time.time()
-                data["aptimes"]=time.mktime(time.strptime(coures["aptimestart"], '%Y-%m-%d %H:%M'))-time.time()
-                data["aptimestart"]=coures["aptimestart"]
-                data["aptimeend"]=coures["aptimeend"]
-                data["notice"]=coures["titile"]
-                data["volume"]=coures["volume"]
-                data["votes"]=coures["votes"]
-                data["titile"]=coures["titile"]
-                data["uuid"]=coures["uuid"]
-                data["topimgV"]=coures["topimgV"]
-                data["customized"]=coures["customized"]
-                shares={}
-                shares["sharetitle"]=coures["sharetitle"]
-                shares["shareimgV"]=coures["shareimgV"]
-                shares["sharedesc"]=coures["sharedesc"]
-                shares["url"]=pojcetm.www+"wx/wxindex?uuid="+coures["uuid"]
+                data = {}
+                data["count"] = count
+                data["endtimes"] = time.mktime(time.strptime(coures["votestart"], '%Y-%m-%d %H:%M')) - time.time()
+                data["aptimes"] = time.mktime(time.strptime(coures["aptimestart"], '%Y-%m-%d %H:%M')) - time.time()
+                data["aptimestart"] = coures["aptimestart"]
+                data["aptimeend"] = coures["aptimeend"]
+                data["notice"] = coures["titile"]
+                data["volume"] = coures["volume"]
+                data["votes"] = coures["votes"]
+                data["titile"] = coures["titile"]
+                data["uuid"] = coures["uuid"]
+                data["topimgV"] = coures["topimgV"]
+                data["customized"] = coures["customized"]
+                shares = {}
+                shares["sharetitle"] = coures["sharetitle"]
+                shares["shareimgV"] = coures["shareimgV"]
+                shares["sharedesc"] = coures["sharedesc"]
+                shares["url"] = pojcetm.www + "wx/wxindex?uuid=" + coures["uuid"]
 
-                aseedata=pojcetm.get_wxcongif(pojcetm.www+self.request.uri)
+                aseedata = pojcetm.get_wxcongif(pojcetm.www + self.request.uri)
 
-                self.render("index.html",data=data,aseedata=aseedata,share=shares)
-    def post(self):
-        pass
+                self.render("index.html", data=data, aseedata=aseedata, share=shares)
 class Getlist(Basehanderl.Basehandelr):
     def post(self):
         key=self.get_argument("keyword")
