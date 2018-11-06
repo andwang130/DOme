07d453567b130bc697dfb2d778b318f7e0e3e6c8
andwang130 <627656470@qq.com>
2018-11-07 03:09:19 +0800

    update

--- wxServer/Handerls/indexHanderl.py
+++ wxServer/Handerls/indexHanderl.py
@@ -15,7 +15,7 @@ class indexHanderl(Basehanderl.Basehandelr):
             url = ' https://api.weixin.qq.com/sns/oauth2/access_token?appid={}&secret={}&code={}&grant_type=authorization_code'.format(pojcetm.wxcongif["appId"],pojcetm.wxcongif["secret"],code)
             print(url)
             http_client = tornado.httpclient.AsyncHTTPClient()
-            response = yield tornado.gen.Task(http_client.fetch, url,validate_cert=False)
+            response = yield tornado.gen.Task(http_client.fetch(url),validate_cert=False)
             print(response.code)
         else :
             return
