de32ef5439eb8049f6ce788e8dfb046906545219
andwang130 <627656470@qq.com>
2018-11-12 11:36:20 +0800

    update

--- wxServer/Handerls/pojcetm.py
+++ wxServer/Handerls/pojcetm.py
@@ -175,7 +175,7 @@ def get_playapImch(price,ip,openid):
         "openid":openid,
     }
     data["sign"]=get_sign({"appid":data["appid"],"mch_id":data["mch_id"],"device_info":data["device_info"],"nonce_str":data["nonce_str"],"body":data["body"]})
-    print(data["sign"])
+    print(data)
     elem = dict_to_xml("xml",data)
     mxl_str=tostring(elem)
     url="https://api.mch.weixin.qq.com/pay/unifiedorder"
