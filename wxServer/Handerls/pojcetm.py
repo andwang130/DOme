1adc93955446fb24fc086a0bc833bc091e3ffb0d
andwang130 <627656470@qq.com>
2018-11-12 11:43:57 +0800

    update

--- wxServer/Handerls/pojcetm.py
+++ wxServer/Handerls/pojcetm.py
@@ -146,7 +146,7 @@ def get_sign(data):
     singstr = ""
     for i in sort_dict:
         singstr += i[0] + "=" + i[1] + "&"
-    singstr = singstr.rstrip("&")+play_Key
+    singstr = singstr.rstrip("&")+"key="+play_Key
     print(singstr)
     newsingstr = hashlib.md5(singstr).hexdigest()
     return newsingstr
