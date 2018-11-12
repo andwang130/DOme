0ff192658fb0ee8bc222cf53a14cecede20473a0
andwang130 <627656470@qq.com>
2018-11-12 13:16:01 +0800

    update

--- wxServer/Handerls/pojcetm.py
+++ wxServer/Handerls/pojcetm.py
@@ -192,7 +192,7 @@ def get_playapImch(price,ip,openid):
     url="https://api.mch.weixin.qq.com/pay/unifiedorder"
     rq=requests.post(url,data=mxl_str)
     rq_xml=rq.content.decode("utf-8")
-    prepay_id=re.findall("<prepay_id><![CDATA[(.*?)]]></prepay_id>")[0]
+    prepay_id=re.findall("<prepay_id><![CDATA[(.*?)]]></prepay_id>",rq_xml)[0]
     print(prepay_id)
 if __name__ == '__main__':
     # print(get_playapImch(100,"127.0.0.1","sdadfgaweqafasfaeaea"))
