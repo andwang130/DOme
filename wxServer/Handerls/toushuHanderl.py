5b7c905c6555a17330c7d9c43a52c209c0b61ba9
andwang130 <627656470@qq.com>
2018-11-14 23:16:16 +0800

    update

--- wxServer/Handerls/toushuHanderl.py
+++ wxServer/Handerls/toushuHanderl.py
@@ -1,13 +1,15 @@
 import Basehanderl
 import time
 import uuid
+import pojcetm
 class toushuHanderl(Basehanderl.Basehandelr):
     def get(self):
-        self.render('toushu.html')
-
+        aseedata = pojcetm.get_wxcongif(pojcetm.www + self.request.uri)
+        self.render('toushu.html',aseedata=aseedata)
     def post(self):
         openid = self.get_secure_cookie("openid")
         if openid:
+            self.db_linck()
             data = {"times": time.time(), "value": openid}
             data["start"] = "openid"
             data["blackid"] = str(uuid.uuid1()).replace("-", "")
