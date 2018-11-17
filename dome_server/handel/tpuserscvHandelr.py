b59e8cb4c5c6274f5c199303328961ddb167544d
andwang130 <627656470@qq.com>
2018-11-17 20:52:07 +0800

    update

---
+++ dome_server/handel/tpuserscvHandelr.py
@@ -0,0 +1,19 @@
+from Basehandelr import Basehandelr
+import csv
+class tpuserscvHandelr(Basehandelr):
+    def get(self):
+        if not self.authen():
+            return
+        uuid_=self.get_argument("uuid","")
+        if uuid_:
+            self.db_linck()
+            coures = self.Mongodb["tpUser"].find({"uuid": uuid_}).sort([("index", -1)])
+            fs=["编号","商家名","号码"]
+            self.set_header("Content-Type", "text/csv")
+            self.set_header('Content-Disposition', 'attachment; filename=%s' %uuid_+".csv" )
+            self.write(",".join(fs))
+            self.write('\r\n')  # 换行
+            for i in coures:
+                vs=[i["index"],i["name"],i["phone"]]
+                self.write(','.join(vs))
+                self.write('\r\n')
