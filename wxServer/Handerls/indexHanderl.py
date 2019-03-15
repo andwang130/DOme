8e39839cb3e068e35bb52dad2e7fe2dab8e5f678
andwang130 <627656470@qq.com>
2019-03-16 02:33:19 +0800

    update

--- wxServer/Handerls/indexHanderl.py
+++ wxServer/Handerls/indexHanderl.py
@@ -82,7 +82,7 @@ class Getlist(Basehanderl.Basehandelr):
         if uuid:
             self.db_linck()
             if key:
-                coures=self.Mongodb["tpUser"].find({"uuid":uuid,"$or":[{"index":key_int},{"name":{"$regex":key}}]}).limit(10).skip(10*(page-1)).sort([(sort,-1)])
+                coures=self.Mongodb["tpUser"].find({"uuid":uuid,"$or":[{"index":key_int},{"name":{"$regex":key}}]}).limit(10).skip(10*(page-1)).sort([(sort,1)])
             else:
                 coures=self.Mongodb["tpUser"].find({"uuid":uuid}).limit(10).skip(10*(page-1)).sort([("index",1)])
             datalist=[]
