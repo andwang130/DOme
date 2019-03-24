7e7d7122edcbe8e096317d7349690b2ff6d1514e
andwang130 <627656470@qq.com>
2019-03-24 23:45:23 +0800

    update

--- autoDome/auto_click.py
+++ autoDome/auto_click.py
@@ -70,10 +70,10 @@ class auto_tp:
     def get_last(self,rank,uuid_,userid):
         cousers=self.Mongodb["tpUser"].find({"uuid":uuid_}).limit(1).skip(rank).sort([("votenum",-1)])
         usercouesr=self.Mongodb["tpUser"].find_one({"uuid":uuid_,"userid":userid})
-        print(usercouesr)
         if usercouesr:
             newlist=[]
             for i in cousers:
+                print(i["votenum"])
                 if usercouesr["votenum"]<i["votenum"]:
                     return i["votenum"]
 
