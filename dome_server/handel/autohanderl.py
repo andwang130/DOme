d842d71f757fee2fef6e83dbb824b5fe7b38d51a
andwang130 <627656470@qq.com>
2019-03-24 14:58:47 +0800

    update

--- dome_server/handel/autohanderl.py
+++ dome_server/handel/autohanderl.py
@@ -118,11 +118,9 @@ class TPhanderl(Basehandelr):
                 useridlist.append(i["userid"])
                 new_tpusrs.append(i)
             print(new_tpusrs)
-            return
             data={"autoid":str(uuid.uuid1()).replace("-",""),"Adminid":self.get_secure_cookie("token"),"times":times,"status":status,
-                  "sort":sort}
-            data={"Adminid":self.get_secure_cookie("token"),"times":times,"uuid":uuid_,"start":start,"end":end,"status":status,"createdate":time.time(),"autoid":str(uuid.uuid1()).replace("-","")}
-            self.Mongodb["autoClick"].insert_one(data)
+                  "sort":sort,"tpusers":new_tpusrs,"createdate":time.time(),"uuid":uuid_}
+            self.Mongodb["autoTP"].insert_one(data)
             self.write(json.dumps({"code":0}))
         else:
             reset["code"]=-1
@@ -130,7 +128,7 @@ class TPhanderl(Basehandelr):
             self.write(json.dumps({"code": -1}))
     def get_list(self):
         page=int(self.get_argument("page",0))
-        coures=self.Mongodb["autoClick"].find({"Adminid":self.get_secure_cookie("token")}).limit(25).skip(25 * (page - 1)).sort([("times", -1)])
+        coures=self.Mongodb["autoTP"].find({"Adminid":self.get_secure_cookie("token")}).limit(25).skip(25 * (page - 1)).sort([("createdate", -1)])
         data_list = []
         for i in coures:
             del i["_id"]
@@ -145,25 +143,50 @@ class TPhanderl(Basehandelr):
     def get_info(self):
         autoid=self.get_argument("autoid","")
         if autoid:
-            coures = self.Mongodb["autoClick"].find_one({"autoid":autoid})
+            coures = self.Mongodb["autoTP"].find_one({"autoid":autoid})
             if coures:
                 del coures["_id"]
                 self.write(json.dumps({"code": 0, "data": coures}))
         else:
             self.write(json.dumps({"code": -1}))
     def update(self):
+        reset={}
         autoid = self.get_argument("autoid", "")
-        uuid = self.get_argument("uuid", "")
-        start = int(self.get_argument("start", ""))
-        end = int(self.get_argument("end", ""))
-        status = int(self.get_argument("status", ""))
-        times=int(self.get_argument("times", ""))
-        if autoid and uuid and start and end and status:
-            data = {"uuid": uuid, "start": start, "end": end, "status": status,"times":times}
-            self.Mongodb["autoClick"].update_one({"autoid":autoid,"Adminid":self.get_secure_cookie("token")},{"$set":data})
-            self.write(json.dumps({"code": 0}))
+        uuid_=self.get_argument("uuid", "")
+        times=int(self.get_argument("times",""))
+        status=int(self.get_argument("status",""))
+        sort=int(self.get_argument("sort",""))
+        if uuid_ and status and times and sort and autoid:
+            tpusers=self.get_argument("tpusers","")
+            if not tpusers:
+                reset["code"]=-1
+                reset["data"]="没有添加用户"
+                return self.write(json.dumps(reset))
+            dict_tpusers=json.loads(tpusers)
+            new_tpusrs=[]
+            useridlist=[]
+            for i in dict_tpusers:
+                i["start"]=int(i["start"])
+                i["end"]=int(i["end"])
+                if i["userid"] in useridlist:
+                    reset["code"]=-1
+                    reset["data"]="重复的用户ID"
+                    self.write(json.dumps(reset))
+                useridlist.append(i["userid"])
+                new_tpusrs.append(i)
+            print(new_tpusrs)
+            data={"autoid":str(uuid.uuid1()).replace("-",""),"Adminid":self.get_secure_cookie("token"),"times":times,"status":status,
+                  "sort":sort,"tpusers":new_tpusrs,"createdate":time.time(),"uuid":uuid_}
+            self.Mongodb["autoTP"].update_one({"autoid": autoid,"Adminid":self.get_secure_cookie("token")},{"$set":data})
+            self.write(json.dumps({"code":0}))
+        else:
+            reset["code"]=-1
+            reset["data"]="缺少数据"
+            self.write(json.dumps({"code": -1}))
+
+
     def delete(self):
         autoid = self.get_argument("autoid", "")
         if autoid:
-            self.Mongodb["autoClick"].delete_one({"autoid": autoid,"Adminid":self.get_secure_cookie("token")})
+            self.Mongodb["autoTP"].delete_one({"autoid": autoid,"Adminid":self.get_secure_cookie("token")})
             self.write(json.dumps({"code": 0}))
--- staticfile/auto_tp.js
+++ staticfile/auto_tp.js
@@ -2,7 +2,6 @@ var tr="<tr>\n" +
     "                \t<input type=\"hidden\" value=\"262592\">\n" +
     "                    <td class=\"text-left vertical-middle\">{间隔}</td>\n" +
     "\t\t\t\t\t<td class=\"text-left vertical-middle\"><span class=\"label label-info\">{活动名称}</span></td>\n" +
-    "\t\t\t\t\t<td class=\"text-left vertical-middle _ticket\"><span class=\"label label-primary _ticket_lable\">{start}-{end}</span></td><!-- 移入移出事件  -->\n" +
     "\t\t\t\t\t<td class=\"text-left vertical-middle\"><p>\n" +
     "\t\t\t\t\t<a class=\"color-default we7-margin-right\" title=\"编辑\" href=\"/addauto_click.html?action=update&autoid={autoid}\"><i class=\"fa fa-edit\"></i> 编辑</a>\n" +
     "\t\t\t\t\t<a class=\"color-default we7-margin-right\" rel=\"tooltip\" href=\"#\" onclick=\"drop_confirm('您确定要删除吗?删除不可恢复，同时删除所有相关数据！','{delete}');\" title=\"删除\"><i class=\"fa fa-times\"></i> 删除</a></p>\n" +
@@ -43,7 +42,7 @@ function get_list(page) {
     data={"action":"list","page":page};
 
      $.ajax({
-        url:'/auto_click',
+        url:'/auto_tp',
         type: 'POST',
         data:data,
         success: function (arg)
@@ -56,7 +55,7 @@ function get_list(page) {
                for(var i=0;i<datalist.length;i++)
                {
                    var new_tr=tr.replace(/{间隔}/,datalist[i]["times"]).replace(/{活动名称}/,datalist[i]["name"])
-                       .replace(/{start}/,datalist[i]["start"]).replace(/{end}/,datalist[i]["end"]).replace(/{autoid}/,datalist[i]["autoid"])
+                       .replace(/{autoid}/,datalist[i]["autoid"])
                        .replace(/{delete}/,datalist[i]["autoid"]);
 
                    if(datalist[i]["status"]==0)
