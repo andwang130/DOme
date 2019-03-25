a3aeb92a5d32e62ee70d80e3ef731c4e2af60351
andwang130 <627656470@qq.com>
2019-03-26 00:55:43 +0800

    update

--- autoDome/auto_click.py
+++ autoDome/auto_click.py
@@ -66,7 +66,7 @@ class auto_tp:
         random.shuffle(tpuserlist)
 
         for i,v in enumerate(tpuserlist):
-            data[i]["userid"]=tpuserlist[i]
+            data["tpusers"][i]["userid"]=tpuserlist[i]
     def nosort_add(self,data):
         pass
     def get_last(self,rank,uuid_,useridlist):
