607471cd86751f5b40cb099f90f8ebb9a81fda37
andwang130 <627656470@qq.com>
2019-04-17 22:10:33 +0800

    update

--- wxServer/Handerls/Basehanderl.py
+++ wxServer/Handerls/Basehanderl.py
@@ -3,7 +3,7 @@ from tornado.web import RequestHandler
 from pymongo import MongoClient
 import tornado
 import urllib
-from Handerls.pojcetm  import wxcongif,www
+from Handerls.pojcetm  import wxcongif,www,chindwww
 import pojcetm
 import json
 import time
@@ -15,7 +15,7 @@ class Basehandelr(RequestHandler):
         self.Mongodb = MongoClient()["Toup"]
 
     def auto(self):
-        values = www + self.request.uri
+        values = chindwww + self.request.uri
         link = urllib.quote(values)
         # link = urljoin(data.scheme + "://" + data.netloc, data.path)
         url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={}&redirect_uri={}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect".format(
