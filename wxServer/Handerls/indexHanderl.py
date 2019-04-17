3ecb5cff636cbc7b9d2c392e01cf536ace1acdeb
andwang130 <627656470@qq.com>
2019-04-17 22:55:06 +0800

    update

--- wxServer/Handerls/indexHanderl.py
+++ wxServer/Handerls/indexHanderl.py
@@ -63,9 +63,9 @@ class indexHanderl(Basehanderl.Basehandelr):
                 shares["sharetitle"] = coures["sharetitle"]
                 shares["shareimgV"] = coures["shareimgV"]
                 shares["sharedesc"] = coures["sharedesc"]
-                shares["url"] = pojcetm.www + "/wx/wxindex?uuid="+uuid_
+                shares["url"] = pojcetm.chindwww + "/wx/wxindex?uuid="+uuid_
                 print(shares)
-                aseedata = pojcetm.get_wxcongif(pojcetm.www + self.request.uri)
+                aseedata = pojcetm.get_wxcongif(pojcetm.chindwww + self.request.uri)
                 if pojcetm.TempCode==1:
                     self.render("index.html", data=data, aseedata=aseedata, share=shares)
                 elif pojcetm.TempCode==2:
