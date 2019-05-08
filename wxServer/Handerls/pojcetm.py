e87fd719bce6a786562eae0f58663934f39fc92b
andwang130 <627656470@qq.com>
2019-05-08 15:53:31 +0800

    update

--- wxServer/Handerls/pojcetm.py
+++ wxServer/Handerls/pojcetm.py
@@ -166,7 +166,7 @@ def creat_dict(root):
         dict_new[key] = dict_init
     return dict_new
 def imgae_change(data):
-    mylist=["himgV","shareimgV","topimgV","topimgV","topimg2V","topimg2V","topimg3V","avatar","images1","images2","images3","images4","images5"]
+    mylist=["himgV","shareimgV","topimgV","topimg2V","topimg2V","topimg3V","avatar","images1","images2","images3","images4","images5"]
     for i in mylist:
         if data.get(i,None):
             try:
@@ -174,6 +174,7 @@ def imgae_change(data):
                 mredis = redis.StrictRedis(**conf_redis)
                 reidisdata=mredis.hgetall("config")
                 data[i]=re.sub(pattern,reidisdata.get("www")+'/',data[i])
+                print(data[i])
             except Exception, e:
 
                 continue
